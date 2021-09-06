3969a4b94045b958a6529dafa5d9611ca673eb71
Midrar <midrar@pdx.edu>
2021-09-05 22:49:07 -0700

    ewh files and plots updated

--- Programmable_Load_Control/scripts/ewh_EnergyTake_emcb_open.py
+++ Programmable_Load_Control/scripts/ewh_EnergyTake_emcb_open.py
@@ -37,6 +37,47 @@ def ewh_energyTake_no_loadup(log):
     ax.annotate('VP Shed Command Received',xy=('18 18:56',0),xytext=('18 09:00',250),arrowprops=dict(arrowstyle='-|>'))
     ax.annotate('VP Shed Command Received (NR)',xy=('18 19:55',0),xytext=('18 12:00',-100),arrowprops=dict(arrowstyle='-|>'))
     #plt.show()
-    plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/ewh/overleaf_figures/ewh_EnergyTake_no_load_up_watts_emcb_open.png")
+    #plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/ewh/overleaf_figures/ewh_EnergyTake_no_load_up_watts_emcb_open.png")
 
-ewh_energyTake_no_loadup(df1)
+#ewh_energyTake_no_loadup(df1)
+
+
+
+
+ewh_log='/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/Data/final_logs/ewh_logs/ewh_emcb_open_load_up.csv'
+
+
+df1 = pd.read_csv(ewh_log)
+
+# This function plots watts consumed from WH station vs watts consumed in VP website
+
+
+def ewh_energyTake_loadup(log):
+    x = log['timestamp']
+    x = pd.to_datetime(log['timestamp'])
+    x = pd.to_datetime(x)
+    x = x.dt.strftime('%H:%M')
+    y = log['real_available_Wh']
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111)
+    ax.xaxis.set_major_locator(plt.MaxNLocator(40))
+    ax.plot(x,y,label='WH Station Data')
+    ax.set_xlabel('time (Hours:minutes)')
+    ax.set_ylabel('EnergyTake (Wh)')
+    plt.xticks(rotation=90)
+    x_max = log['timestamp'].max()
+    ax.set_xlim([0,x_max]) 
+    ax.set_title('EnergyTake VS Time')
+    ax.legend(loc='upper right')
+    ax.annotate('VP Shed Command Received',xy=('06:39',75),xytext=('07:23',-100),arrowprops=dict(arrowstyle='-|>'))
+    ax.annotate('VP Shed Command Received',xy=('18:55',150),xytext=('14:48',-100),arrowprops=dict(arrowstyle='-|>'))
+    ax.annotate('VP Shed Command Received',xy=('19:56',75),xytext=('14:48',-100),arrowprops=dict(arrowstyle='-|>'))
+
+    ax.annotate('Loadup Command Received',xy=('06:16',750),xytext=('00:40',1000),arrowprops=dict(arrowstyle='-|>'))
+    ax.annotate('Loadup Command Received',xy=('18:16',825),xytext=('14:00',1000),arrowprops=dict(arrowstyle='-|>'))
+
+
+    #plt.show()
+    plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/ewh/overleaf_figures/ewh_EnergyTake_load_up_emcb_open.png")
+
+ewh_energyTake_loadup(df1)
--- Programmable_Load_Control/scripts/hpwh_energy_take_emcb_open.py
+++ Programmable_Load_Control/scripts/hpwh_energy_take_emcb_open.py
@@ -31,6 +31,42 @@ def hpwh_energyTake_loadup(log):
     ax.annotate('VP Shed Command Received',xy=('18:54',150),xytext=('14:07',300),arrowprops=dict(arrowstyle='-|>'))
     ax.annotate('VP Shed Command Received',xy=('19:54',450),xytext=('14:07',300),arrowprops=dict(arrowstyle='-|>'))
     #plt.show()
-    plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/hpwh/overleaf_figures/hpwh_no_load_up_energyTake_emcb_open.png")
+    #plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/hpwh/overleaf_figures/hpwh_no_load_up_energyTake_emcb_open.png")
+#hpwh_energyTake_loadup(df1)
+
+
+
+hpwh_log="/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/Data/final_logs/hpwh_logs/hpwh_emcb_open_load_up.csv"
+
+df1 = pd.read_csv(hpwh_log)
+
+# This function plots energyTake closed emcb for hpwh
+
+
+def hpwh_energyTake_loadup(log):
+    x = log['timestamp']
+    x = pd.to_datetime(log['timestamp'])
+    x = pd.to_datetime(x)
+    x = x.dt.strftime('%H:%M')
+    y = log['real_available_Wh']
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111)
+    ax.xaxis.set_major_locator(plt.MaxNLocator(40))
+    ax.plot(x,y,label='WH Station Data')
+    ax.set_xlabel('time (Hours:minutes)')
+    ax.set_ylabel('EnergyTake (Wh)')
+    plt.xticks(rotation=90)
+    x_max = log['timestamp'].max()
+    ax.set_xlim([0,x_max]) 
+    ax.set_title('EnergyTake VS Time')
+    ax.legend(loc='upper right')
+    ax.annotate('Loadup Command Received',xy=('05:31',525),xytext=('00:41',700),arrowprops=dict(arrowstyle='-|>'))
+
+    ax.annotate('VP Shed Command Received',xy=('06:41',0),xytext=('02:01',-100),arrowprops=dict(arrowstyle='-|>'))
+    ax.annotate('VP Shed Command Received',xy=('18:55',150),xytext=('14:07',-100),arrowprops=dict(arrowstyle='-|>'))
+    ax.annotate('VP Shed Command Received',xy=('19:56',1275),xytext=('14:07',1275),arrowprops=dict(arrowstyle='-|>'))
+
+    #plt.show()
+    plt.savefig("/home/parallels/Desktop/emcb-use-cases/Programmable_Load_Control/figures/hpwh/overleaf_figures/hpwh_load_up_energyTake_emcb_open.png")
 hpwh_energyTake_loadup(df1)
 
